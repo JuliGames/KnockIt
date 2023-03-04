48839fcbafbd429b4a4d6b3a52ee2133895d589d
TDR <tdr.minecraft@gmail.com>
2023-03-04 10:25:14 +0100

    add initial project

---
+++ src/main/java/net/juligames/knockit/KnockItPlugin.java
@@ -0,0 +1,17 @@
+package net.juligames.knockit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KnockItPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
