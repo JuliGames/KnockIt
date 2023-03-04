dc8e318ad189104683d819b6e0e1cffc39ae20f0
TDR <tdr.minecraft@gmail.com>
2023-03-04 10:38:50 +0100

    add multiverse

--- src/main/java/net/juligames/knockit/KnockItPlugin.java
+++ src/main/java/net/juligames/knockit/KnockItPlugin.java
@@ -1,12 +1,35 @@
 package net.juligames.knockit;
 
+import com.onarandombox.MultiverseCore.MultiverseCore;
+import com.onarandombox.MultiverseCore.api.MVWorldManager;
+import net.juligames.core.api.API;
+import net.juligames.core.api.message.MessageApi;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public final class KnockItPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        //registerMessages
+        registerMessages(API.get().getMessageApi());
+    }
+
+    public static @NotNull MultiverseCore getMultiverseCore() {
+        return (MultiverseCore) Objects.requireNonNull(Bukkit.getServer().
+                getPluginManager().getPlugin("Multiverse-Core"));
+    }
+
+    public static @NotNull MVWorldManager getMVWorldManager() {
+        return getMultiverseCore().getMVWorldManager();
+    }
+
+    private void registerMessages(@NotNull MessageApi messageApi) {
 
     }
 
